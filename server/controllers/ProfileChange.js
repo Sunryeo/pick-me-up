3e1a137282aa98af7580a0bb762f42e95770af7b
Sunryeo <elma9700@gmail.com>
2021-09-02 00:53:20 +0900

    dev

--- server/controllers/ProfileChange.js
+++ server/controllers/ProfileChange.js
@@ -1,17 +1,14 @@
-const express = require("express");
 const { users } = require("../models");
-const jwt = require("jsonwebtoken");
 const { isAuthorized } = require("./tokenFunction");
-require("dotenv").config();
+const { generateAccessToken } = require("./tokenFunction");
 
 module.exports = {
   // [POST] /user/profile/:id
   changeProfile: (req, res) => {
     const user_id = req.params.id;
     console.log("user_id", user_id);
-    // const authorization = req.headers.cookie;
-    // console.log(authorization);
-    const { userName, mobile } = req.body;
+
+    const { userName, mobile, password } = req.body;
 
     const accessTokendata = isAuthorized(req);
     console.log(accessTokendata);
@@ -20,31 +17,70 @@ module.exports = {
       res.status(401).json({ message: "invalid access token" });
     } else {
       // res.status(200).json({ message: "ok" });
-      users
-        .update(
-          {
-            nickname: userName,
-            phone_number: mobile,
-          },
-          {
-            where: {
-              id: accessTokendata.id,
+      // password 변경 시
+      if (password.length !== 0) {
+        const newPasswordToken = generateAccessToken(password);
+        users
+          .update(
+            {
+              nickname: userName,
+              phone_number: mobile,
+              password: newPasswordToken,
             },
-          }
-        )
-        .then((userInfo) => {
-          console.log("userInfo:", userInfo);
-          if (!userInfo) {
-            res.status(404).json({ message: "user not exists" });
-          } else {
-            // console.log("userInfo:", userInfo);
-            res.status(200).json({ message: "profile changed" });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+            {
+              where: {
+                id: accessTokendata.id,
+              },
+            }
+          )
+          .then((userInfo) => {
+            console.log("userInfo:", userInfo);
+            if (!userInfo) {
+              res.status(404).json({ message: "user not exists" });
+            } else {
+              // console.log("userInfo:", userInfo);
+              delete userInfo.dataValues.password;
+              const newAccessToken = generateAccessToken(userInfo);
+              res
+                .status(200)
+                .json({ data: newAccessToken, message: "profile changed" });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      // password 변경 없을 시
+      else {
+        users
+          .update(
+            {
+              nickname: userName,
+              phone_number: mobile,
+            },
+            {
+              where: {
+                id: accessTokendata.id,
+              },
+            }
+          )
+          .then((userInfo) => {
+            console.log("userInfo:", userInfo);
+            if (!userInfo) {
+              res.status(404).json({ message: "user not exists" });
+            } else {
+              // console.log("userInfo:", userInfo);
+              delete userInfo.dataValues.password;
+              const newAccessToken = generateAccessToken(userInfo);
+              res
+                .status(200)
+                .json({ data: newAccessToken, message: "profile changed" });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     }
   },
 };
-
